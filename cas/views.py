7a9200ae0330912bd0abb5ee3a049121b177e096
Edward <edwardxiao29@gmail.com>
2022-07-11 22:37:50 -0400

    fix doc typo

--- cas/views.py
+++ cas/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from umd_python_cas import UMDCASClient
 
 
-#hostname and post authentication flask route must be passed in to generate CAS login url
+#host name and post authentication Django route must be passed in to generate CAS login url
 client = UMDCASClient(host_name="http://127.0.0.1:8000/", post_auth_route="/secure")
 
 #default route to load html
